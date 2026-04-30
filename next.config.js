098092e473344dd030185a64cd36a8c528b9468a
Valthre <eduentidade@gmail.com>
2026-04-30 15:15:54 -0300

    Enhances CSP headers; removes redundant headers

--- next.config.js
+++ next.config.js
@@ -1,28 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; connect-src 'self'; object-src 'none'; base-uri 'self';",
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
-  }
-];
-
 const nextConfig = {
   output: 'export',
   images: {
@@ -31,14 +7,7 @@ const nextConfig = {
   allowedDevOrigins: [
     'https://*.cloudworkstations.dev'
   ],
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
+  
 };
 
 module.exports = nextConfig;
